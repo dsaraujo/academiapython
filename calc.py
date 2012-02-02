2c8e58a298dae57ba7d200b5a593307aa0f8a240
Daniel Araujo <daniel.anand@gmail.com>
2012-02-02 14:32:27 -0200

    Calc em OO

--- calc.py
+++ calc.py
@@ -1,36 +1,73 @@
 # coding: utf-8
-# Simula uma calculadora (extensível) de boteco
-
-import operator as op # Usando operator pra facilitar a vida
-
+# Simulates an extensible calculator (now using OO)
 __author__ = 'Daniel Araujo'
 
-def menosPorcento(a,b):
-  return a-(a*(b/100))
+import operator as op
 
-def maisPorcento(a,b):
-  return a+(a*(b/100))
+class Calculator(object):
+    
+    standard_ops = { # Default operations
+    '+':op.add,
+    '-':op.sub,
+    '*':op.mul,
+    '/':op.div,
+    '**':op.pow
+    }
+    
+    def __init__(self):
+        self.ops = Calculator.standard_ops
+        self.currentop = self.ops['+']
+        self.total = 0
+    
+    def processLine(self, line):
+        """Process the line and returns a message (can be empty)"""
+        if line == '\x1b':                          # \x1b =>  Esc
+            self.currentop = self.ops['+']
+            self.total = 0
+            return 'Clr'
+        elif self.ops.has_key(line):
+            self.currentop = self.ops[line]
+        else:
+            err = ''
+            try:
+                f = float(line)
+                self.total = self.currentop(self.total, f)
+            except ValueError:
+                err = 'Err: %s' % line
+            return err
+    
+    def addOp(self, key, func):
+        # TODO: Test if func can only receives 2 floats arguments and
+        #       returns a float, otherwise throws an Exception
+        self.ops[key] = func
+    
+    def getOp(self):
+        """Return the string of the current operation, empty string if not found"""
+        for k in self.ops.keys():
+            if self.ops[k] is self.currentop:
+				return k
+        return ''
 
-total = 0.0
-ops = {'+':op.add, '-':op.sub, '*':op.mul, '/':op.div,'**':op.pow}
- 
-ops['-%'] = menosPorcento                  # Para extender, adicione expressões desta maneira
-ops['+%'] = maisPorcento
 
-current = '+'                              # Inicia a calculadora com soma (ops[0])
-while True:
-  print total
-  p = raw_input(current)
-  if not p.strip(): break
-  if p == '\x1b':                          # \x1b => tecla Esc
-    total = 0
-    print 'Clr'                            # Imprime Clr (Clear) e volta pra soma
-    current = '+'
-  elif p in ops:
-    current = p                            # Troca a operação
-  else: 
-    try:
-      f = float(p)
-      total = ops[current](total, f)       # Eval resolve a expressão como um comando
-    except ValueError:                     # Jogada por float(str)
-      print 'Err:',p                       # Imprime Err (Error)
+if __name__ == "__main__":
+    
+    def lessPercent(a,b):
+        """Reduces the value a in b%. Returns the reduced value.
+        Ex: lessPercent(100, 10) returns 100-10%, 90."""
+        return a-(a*(b/100))
+    
+    def plusPercent(a,b):
+        """Increases the value a in b%. Returns the reduced value.
+        Ex: lessPercent(100, 10) returns 100+10%, 110."""
+        return a+(a*(b/100))
+    
+    c = Calculator()   # Instantiate a new calculator
+    c.addOp('-%', lessPercent)  # Customize it with additional operations
+    c.addOp('+%', plusPercent)
+    
+    while True:
+        print c.total
+        p = raw_input(c.getOp())
+        if not p.strip(): break
+        out = c.processLine(p)
+        if out: print out 
